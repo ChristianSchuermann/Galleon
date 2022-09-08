b85d156c2f51c207a0b754d6c1dbbc786cd66e59
Christian Schuermann <christian.schuermann.90@gmail.com>
2022-09-08 14:02:35 +0200

    fixed typo

--- galleon-server/models/Wallet.model.js
+++ galleon-server/models/Wallet.model.js
@@ -4,11 +4,9 @@ const { Schema, model } = mongoose;
  
 const WalletSchema = new Schema({
 
-  email: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  expense: { type: Schema.Types.ExpenseID, red: "Expense" }
+  income: { type: Schema.Types.IncomeID, red: "Income" },
+  expense: { type: Schema.Types.ExpenseID, red: "Expense" },
+  user: {type: Schema.Types.ExpenseID, red: "User"}
 
 });
  
