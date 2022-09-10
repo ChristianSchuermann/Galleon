a3f3d57a0d1f35a8b82a6dab01da14accadff697
Christian Schuermann <christian.schuermann.90@gmail.com>
2022-09-10 12:31:55 +0200

    fixed colors

--- galleon-client/src/Components/Footer.js
+++ galleon-client/src/Components/Footer.js
@@ -4,23 +4,10 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="bg-violet-600 text-white">
-      <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-violet-600 py-7">
-        <h1> </h1>
-        <h1> </h1>
-        <h1> Ece Çakmak </h1>
+    <footer className="bg-[#7F3DFF] text-white">
+      <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#7F3DFF] py-7">
         <h1 className="lg:text-4x1 text-3x1 md:mb-0 mb-6 lg:leading-normal font-semibold md:w-2/5">
-          <span className="text-yellow-600">Github</span>
-        </h1>
-        <h1 className="lg:text-4x1 text-3x1 md:mb-0 mb-6 lg:leading-normal font-semibold md:w-2/5">
-          <span className="text-yellow-600">LinkedIn</span>
-        </h1>
-        <h1> Christian Schuermann </h1>
-        <h1 className="lg:text-4x1 text-3x1 md:mb-0 mb-6 lg:leading-normal font-semibold md:w-2/5">
-          <span className="text-yellow-600">Github</span>
-        </h1>
-        <h1 className="lg:text-4x1 text-3x1 md:mb-0 mb-6 lg:leading-normal font-semibold md:w-2/5">
-          <span className="text-yellow-600">LinkedIn</span>
+          <span className="text-[#FCAC12]">Github</span>
         </h1>
       </div>
     </footer>
