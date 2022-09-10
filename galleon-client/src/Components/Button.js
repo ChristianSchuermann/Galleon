9c3b6162940acc39a47798f18b63392e19011ab9
Ece Çakmak <86318154+ececmk@users.noreply.github.com>
2022-09-10 15:31:10 +0200

    update button, expenseCard,IncomeCard and HomePage

--- galleon-client/src/Components/Button.js
+++ galleon-client/src/Components/Button.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export const Button = (props) => {
   return (
     <button
-      className="bg-[#FCAC12] text-black font-[Poppins] py-2 px-6 rounded md:ml-8 hover:bg-yellow-400
+      className="bg-[#FFFFFF] text-[#7F3DFF] font-[Poppins] py-2 px-6 rounded md:ml-8 hover:bg-purple-400
     duration-500"
     >
       ʛ Get Started
--- galleon-client/src/Pages/Home.js
+++ galleon-client/src/Pages/Home.js
@@ -3,47 +3,48 @@ import financeImg1 from '../Images/1.jpg'
 import financeImg2 from '../Images/2.jpg'
 import financeImg3 from '../Images/3.jpg';
 
-function Home() {
+function Home(props) {
   return (
 
 
     <>
-    <div className="container mx-auto  px-4 bg-[#7F3DFF] mt-40 flex w-2/3 h-3/4  justify-between rounded-md">
+    <div className="container mx-auto  px-4 bg-[#7F3DFF] mt-20 flex w-2/3 h-3/4  justify-between rounded-md pt-10 pb-20">
       <div className='pt-10 pl-5'>
         <h1 className='md:text-6xl lg:text-8xl text-white mb-8 max-w-md font-bold'>Need help about savings?</h1>
         <h2 className='md:text-2xl lg:text-4xl text-white mb-8 max-w-md'>Galleon here to help!</h2>
-        <button>Let's get started!</button>
+        <button className="bg-[#FFFFFF] text-[#7F3DFF] font-[Poppins] py-2 px-6 rounded md:ml-8 hover:bg-purple-400
+    duration-500">
+      Let's get started!
+      {props.children}
+    </button>
       </div>
       <img className='pb-20 items-center' src={piggyBank} alt="Piggy Bank" />
     </div>
-    <div className="columns-3 flex place-content-evenly mt-40">
+    <div className="columns-3 flex place-content-evenly mt-40 mb-20">
     <div>
-      <img className='w-80 h-60' src={financeImg1}/>
+      <img className='w-80 h-60' src={financeImg1} alt="finance pic"/>
       <div className='w-80 bg-[#7F3DFF] h-72 rounded-xl m-5 p-10  flex flex-col  items-center  ' >
         <h1 className='md:text-2xl lg:text-3xl text-white font-bold'>It's easy to manage!</h1>
         <p className='md:text-lg lg:text-xl text-white'>Track your financial situation through a single application.</p>
       </div>
     </div>
     <div>
-    <img className='w-80 h-60'  src={financeImg2}/>
+    <img className='w-80 h-60'  src={financeImg2} alt="finance pic"/>
     <div className='w-80 bg-[#7F3DFF] h-72 rounded-xl m-5 p-10 flex flex-col  items-center'>
         <h1 className='md:text-2xl lg:text-3xl text-white font-bold'>Increase your savings!</h1>
         <p className='md:text-lg lg:text-xl text-white'>Controlling your spending is easier than you think.</p>
     </div>
     </div>
     <div>
-    <img className='w-80 h-60'  src={financeImg3}/>
+    <img className='w-80 h-60'  src={financeImg3} alt="finance pic"/>
     <div className='w-80 bg-[#7F3DFF] h-72 rounded-xl m-5 p-10 flex flex-col items-center'>
         <h1 className='md:text-2xl lg:text-3xl text-white font-bold'>Enjoy your money!</h1>
         <p className='md:text-lg lg:text-xl text-white'>Realize your dreams thanks to the savings you make.</p>
     </div>
     </div>
-
     </div>
       
       </>
-
-
   );
 }
 
