47d697850bfac39fe69b5e76e91b0e6aa5bdecdd
Ece Çakmak <86318154+ececmk@users.noreply.github.com>
2022-09-12 16:02:11 +0200

    homepage css done

--- galleon-client/src/Pages/Home.js
+++ galleon-client/src/Pages/Home.js
@@ -6,12 +6,13 @@ import financeImg3 from "../Images/3.jpg";
 function Home(props) {
   return (
     <>
-      <div className="container mx-auto  px-4 bg-[#7F3DFF] mt-20 flex w-2/3 h-3/4  justify-between rounded-md pt-10 pb-20">
+    <div className="flex justify-center flex-col">
+      <div className="container mx-auto  px-4 bg-[#7F3DFF] mt-20 flex w-2/3 h-3/4  justify-between rounded-md pt-10 pb-20 flex-col lg:flex-row">
         <div className="pt-10 pl-5">
-          <h1 className="md:text-6xl lg:text-8xl text-white mb-8 max-w-md font-bold">
+          <h1 className="text-6xl lg:text-8xl text-white mb-8  font-bold">
             Need help about savings?
           </h1>
-          <h2 className="md:text-2xl lg:text-4xl text-white mb-8 max-w-md">
+          <h2 className="md:text-2xl lg:text-4xl text-white mb-8">
             Galleon here to help!
           </h2>
           <button
@@ -22,16 +23,16 @@ function Home(props) {
             {props.children}
           </button>
         </div>
-        <img className="pb-20 items-center" src={piggyBank} alt="Piggy Bank" />
+        <img className="pb-20 items-center " src={piggyBank} alt="Piggy Bank" />
       </div>
-      <div className="columns-3 flex place-content-evenly mt-40 mb-20">
+      <div className="columns-1 lg:columns-3  mt-40 mb-20 self-center lg:items-center" >
         <div>
           <img className="w-80 h-60" src={financeImg1} alt="finance pic" />
           <div className="w-80 bg-[#7F3DFF] h-72 rounded-xl m-5 p-10  flex flex-col  items-center  ">
-            <h1 className="md:text-2xl lg:text-3xl text-white font-bold">
+            <h1 className="text-3xl lg:text-4xl text-white font-bold">
               It's easy to manage!
             </h1>
-            <p className="md:text-lg lg:text-xl text-white">
+            <p className="text-lg lg:text-xl text-white">
               Track your financial situation through a single application.
             </p>
           </div>
@@ -39,7 +40,7 @@ function Home(props) {
         <div>
           <img className="w-80 h-60" src={financeImg2} alt="finance pic" />
           <div className="w-80 bg-[#7F3DFF] h-72 rounded-xl m-5 p-10 flex flex-col  items-center">
-            <h1 className="md:text-2xl lg:text-3xl text-white font-bold">
+            <h1 className="text-3xl lg:text-4xl text-white font-bold">
               Increase your savings!
             </h1>
             <p className="md:text-lg lg:text-xl text-white">
@@ -50,7 +51,7 @@ function Home(props) {
         <div>
           <img className="w-80 h-60" src={financeImg3} alt="finance pic" />
           <div className="w-80 bg-[#7F3DFF] h-72 rounded-xl m-5 p-10 flex flex-col items-center">
-            <h1 className="md:text-2xl lg:text-3xl text-white font-bold">
+            <h1 className="text-3xl lg:text-4xl text-white font-bold">
               Enjoy your money!
             </h1>
             <p className="md:text-lg lg:text-xl text-white">
@@ -58,6 +59,8 @@ function Home(props) {
             </p>
           </div>
         </div>
+      </div>
+
       </div>
     </>
   );
