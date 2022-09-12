69eda42a0bf690c88b6ad418fa4d979d0b4fcd94
Christian Schuermann <christian.schuermann.90@gmail.com>
2022-09-12 12:14:35 +0200

    typos ...

--- galleon-client/src/Pages/ExpenseList.js
+++ galleon-client/src/Pages/ExpenseList.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-import IncomeCard from "../Components/IncomeCard";
+import ExpenseCard from "../Components/ExpenseCard";
 import AddExpense from "../Components/AddExpense";
 
 const API_URL = "http://localhost:5005";
@@ -11,31 +11,29 @@ function ExpenseListPage() {
 
   const getExpense = () => {
     const storedToken = localStorage.getItem("authToken");
- 
-  // Send the token through the request "Authorization" Headers
-  axios
-    .get(
-    `${API_URL}/api/expense`,
-    { headers: { Authorization: `Bearer ${storedToken}` } }
-  )
-    .then((response) => setExpense(response.data))
-    .catch((error) => console.log(error));
+
+    // Send the token through the request "Authorization" Headers
+    axios
+      .get(`${API_URL}/api/expense`, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
+      .then((response) => setExpense(response.data))
+      .catch((error) => console.log(error));
   };
 
   // We set this effect will run only once, after the initial render
   // by setting the empty dependency array - []
   useEffect(() => {
     getExpense();
-  }, [] );
+  }, []);
 
-  
   return (
     <div>
-      
       <AddExpense refreshProjects={getExpense} />
-      
-      { expense.map((income) => <IncomeCard key={income._id} {...income} />  )} 
-       
+
+      {expense.map((income) => (
+        <ExpenseCard key={expense._id} {...expense} />
+      ))}
     </div>
   );
 }
