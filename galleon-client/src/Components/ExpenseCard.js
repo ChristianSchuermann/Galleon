286bda9f65a4b3c1590b3c9dd88a9b87f39304fc
Ece Çakmak <86318154+ececmk@users.noreply.github.com>
2022-09-13 13:19:28 +0200

    Add edit button to expensecard and incomecard

--- galleon-client/src/Components/ExpenseCard.js
+++ galleon-client/src/Components/ExpenseCard.js
@@ -4,6 +4,7 @@ import expenseIcon from "../Images/icons8-shopping-50.png";
 // We are deconstructing props object directly in the parentheses of the function
 function ExpenseCard({ title, description, expense, category, _id }) {
   return (
+    <>
     <div className="box-border py-10 w-60  rounded-3xl flex items-center justify-center bg-[#FD3C4A] ">
     <div>
       <img className="pr-5" src={expenseIcon}  alt="Expense Icon"/>
@@ -17,6 +18,8 @@ function ExpenseCard({ title, description, expense, category, _id }) {
       <p className="text-lg lg:text-xl text-white">{category}</p>
     </div>
     </div>
+    <Link to="/EditExpense"><button className="bg-[#FD3C4A] text-[#FFFFFF] flex items-center justify-center py-2 px-4 rounded  hover:bg-red-400 duration-500 mt-2  ">Edit Expense</button></Link>
+    </>
   );
 }
 
