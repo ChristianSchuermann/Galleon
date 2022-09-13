521e6948c42f6024c00a9b7fdef29d009ddebbce
Ece Çakmak <86318154+ececmk@users.noreply.github.com>
2022-09-13 19:30:00 +0200

    update signup url

--- galleon-client/src/Pages/Signup.js
+++ galleon-client/src/Pages/Signup.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import galleons from "../Images/galleons.png";
 
 const API_URL = "http://localhost:5005";
 
@@ -25,7 +24,7 @@ function Signup(props) {
     const requestBody = { email, password, firstName, lastName };
 
     axios
-      .post(`${API_URL}/auth/signup`, requestBody)
+      .post(`http://localhost:5005/auth/signup`, requestBody)
       .then((response) => {
         navigate("/login");
       })
@@ -37,13 +36,10 @@ function Signup(props) {
 
   return (
     <div className="grid grid-cols-1 mx">
-      <img
-        className="w-86 h-86 z-10 ml-64 absolute "
-        src={galleons}
-        alt="Galleons"
-      />
 
-      <div className=" flex flex-col justify-center z-20 mt-16 mb-40">
+ 
+     
+      <div className=" flex flex-col justify-center  mt-16 mb-40">
         <form
           className="max-w-[500px]  max-h-[400] w-full mx-auto bg-[#7F3DFF] p-12 px-11 rounded-lg"
           onSubmit={handleSignupSubmit}
@@ -102,12 +98,7 @@ function Signup(props) {
           <div className="flex justify-center text-white">
             <p>Already have an account?</p>
 
-            <Link
-              className="text-white hover:text-violet-400 ml-2"
-              to={"/login"}
-            >
-              ʛ Login
-            </Link>
+            <Link className="text-white hover:text-violet-400 ml-2" to={"/login"}>ʛ Login</Link>
           </div>
         </form>
         {errorMessage && <p className="error-message">{errorMessage}</p>}
